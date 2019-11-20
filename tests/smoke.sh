7e0fb654b2feecdded75d0af5d01f9bd0cd90e8b
hakre <hanskrentel@yahoo.de>
2019-11-20 23:09:00 +0100

    less smoke on git archive
    when fiddling w/ git attributes configuration in light of creating
    packages w/ `$ git archive ...` it's nice to see whether or not uncommitted
    changes create the wished result or not.
    
    therefore the smoke test for a fine package is done before the real test
    with the same test but taking the working tree git attributes into account.

--- tests/smoke.sh
+++ tests/smoke.sh
@@ -19,6 +19,13 @@ echo "# files with no newline at the end of file are skipped"
 git ls-files '*/*newline*.php' | pcre.php '/FOO/'
 [[ $? -eq 0 ]]
 
+echo "# packaging with git archive (worktree attributes)"
+<<EOD diff <( git archive --worktree-attributes HEAD | tar -t ) -
+README.md
+pcre.php
+EOD
+[[ $? -eq 0 ]]
+
 echo "# packaging with git archive"
 <<EOD diff <( git archive HEAD | tar -t ) -
 README.md
