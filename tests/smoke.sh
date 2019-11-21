18cb0515aa1f4059c46fcc134f7e12c754e2831c
hakre <hanskrentel@yahoo.de>
2019-11-21 22:41:32 +0100

    fix smoke tests
    exist condition was not given.
    
    fix is straight forward by exiting in case of failure.

--- tests/smoke.sh
+++ tests/smoke.sh
@@ -11,30 +11,30 @@ echo "sort..: $(sort --version | head -n 1)"
 
 echo "# wrong argument gives error"
 ./pcre.php --faux-long-option 1>/dev/null
-[[ $? -eq 1 ]]
+[[ $? -eq 1 ]] || exit 1
 
 echo "# works with git-ls-files in quote mode"
 git ls-files '*.php' | ./pcre.php 2>&1
-[[ $? -eq 0 ]]
+[[ $? -eq 0 ]] || exit 1
 
 echo "# files with no newline at the end of file are skipped"
 git ls-files '*/*newline*.php' | pcre.php '/FOO/'
-[[ $? -eq 0 ]]
+[[ $? -eq 0 ]] || exit 1
 
 echo "# works with including (unit tests)"
 tests/include.php
-[[ $? -eq 0 ]]
+[[ $? -eq 0 ]] || exit 1
 
 echo "# packaging with git archive (worktree attributes)"
 <<EOD diff <( git archive --worktree-attributes HEAD | tar -t ) -
 README.md
 pcre.php
 EOD
-[[ $? -eq 0 ]]
+[[ $? -eq 0 ]] || exit 1
 
 echo "# packaging with git archive"
 <<EOD diff <( git archive HEAD | tar -t ) -
 README.md
 pcre.php
 EOD
-[[ $? -eq 0 ]]
+[[ $? -eq 0 ]] || exit 1
